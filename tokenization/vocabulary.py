340f2e68d33d82ad7873eb1eca07c3500a870670
Connor <connorkasarda@gmail.com>
2025-04-13 13:16:23 -0500

    simple tokenizer added.

--- tokenization/vocabulary.py
+++ tokenization/vocabulary.py
@@ -10,7 +10,14 @@ class Vocabulary:
     A class to manage a vocabulary for tokenization.
     """
 
-    def __init__(self, max_size: int = 10000, UNKNOWN_TOKEN: str = '<UNK>', PADDING_TOKEN: str = '<PAD>') -> None:
+    def __init__(
+        self,
+        max_size: int = 10000,
+        UNKNOWN_TOKEN: str = '<UNK>',
+        PADDING_TOKEN: str = '<PAD>',
+        BEGIN_OF_SEQ_TOKEN: str = '<BOS>',
+        END_OF_SEQ_TOKEN: str = '<EOS>'
+    ) -> None:
         """
         Initializes an empty Vocabulary.
 
@@ -18,13 +25,19 @@ class Vocabulary:
             max_size (int): The maximum size of the vocabulary.
             UNKNOWN_TOKEN (str): The token representing unknown words.
             PADDING_TOKEN (str): The token representing padding.
+            BEGIN_OF_SEQ_TOKEN (str): The token representing the beginning of a sequence.
+            END_OF_SEQ_TOKEN (str): The token representing the end of a sequence.
         
         Attributes:
             max_size (int): The maximum size of the vocabulary.
             UNKNOWN_TOKEN (str): The token representing unknown words.
             PADDING_TOKEN (str): The token representing padding.
+            BEGIN_OF_SEQ_TOKEN (str): The token representing the beginning of a sequence.
+            END_OF_SEQ_TOKEN (str): The token representing the end of a sequence.
             UNKNOWN_ID (int): The ID for the unknown token.
             PADDING_ID (int): The ID for the padding token.
+            BEGIN_OF_SEQ_ID (int): The ID for the beginning of sequence token.
+            END_OF_SEQ_ID (int): The ID for the end of sequence token.
             token_2_id_map (dict): A mapping from tokens to their unique IDs.
             id_2_token_map (dict): A mapping from unique IDs to their corresponding tokens.
         """
@@ -32,8 +45,12 @@ class Vocabulary:
         self.max_size = max_size
         self.UNKNOWN_TOKEN = UNKNOWN_TOKEN
         self.PADDING_TOKEN = PADDING_TOKEN
+        self.BEGIN_OF_SEQ_TOKEN = BEGIN_OF_SEQ_TOKEN
+        self.END_OF_SEQ_TOKEN = END_OF_SEQ_TOKEN
         self.UNKNOWN_ID = None
         self.PADDING_ID = None
+        self.BEGIN_OF_SEQ_ID = None
+        self.END_OF_SEQ_ID = None
         self.token_2_id_map = {}
         self.id_2_token_map = {}
 
@@ -49,7 +66,7 @@ class Vocabulary:
         if tokens is None:
             tokens = []
 
-        special_tokens = [self.UNKNOWN_TOKEN, self.PADDING_TOKEN]
+        special_tokens = [self.UNKNOWN_TOKEN, self.PADDING_TOKEN, self.BEGIN_OF_SEQ_TOKEN, self.END_OF_SEQ_TOKEN]
         unique_tokens = sorted(set(tokens) - set(special_tokens))
         limited_tokens = unique_tokens[:self.max_size - len(special_tokens)]
         all_tokens = special_tokens + limited_tokens
@@ -59,6 +76,8 @@ class Vocabulary:
 
         self.UNKNOWN_ID = self.token_2_id_map[self.UNKNOWN_TOKEN]
         self.PADDING_ID = self.token_2_id_map[self.PADDING_TOKEN]
+        self.BEGIN_OF_SEQ_ID = self.token_2_id_map[self.BEGIN_OF_SEQ_TOKEN]
+        self.END_OF_SEQ_ID = self.token_2_id_map[self.END_OF_SEQ_TOKEN]
 
     def get_id(self, token: str) -> int:
         """
