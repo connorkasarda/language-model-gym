05bc1df7945b7ddaee8f066d61eae08b26330fd5
Connor Kasarda <connorkasarda@gmail.com>
2025-04-26 22:14:56 -0500

    added foundation for word piece unit testing.

---
+++ tests/test_word_piece.py
@@ -0,0 +1,15 @@
+"""
+Name: test_word_piece.py
+Description: Unit tests for the WordPiece tokenizer algorithm
+Author: Connor Kasarda
+Date: 2025-04-26
+
+Warning:
+    Use at your own risk. The author is not responsible for any damages or losses incurred from using this code.
+"""
+
+import unittest
+from tokenization.word_piece import WordPiece
+
+class TestWordPiece(unittest.TestCase):
+    pass
